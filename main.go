a0926a3be64e3451c35f26a013760ecb03d7848c
ろむねこ <nem@nem.x0.to>
2024-05-06 12:16:00 +0900

    テンプレートから生成

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"nem.one/written-drafts-utils/internal/handler"
+	"nem.one/written-drafts-utils/internal/migration"
+	"nem.one/written-drafts-utils/internal/pkg/config"
+	"nem.one/written-drafts-utils/internal/repository"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+
+	// middlewares
+	e.Use(middleware.Recover())
+	e.Use(middleware.Logger())
+
+	// connect to database
+	db, err := sqlx.Connect("mysql", config.MySQL().FormatDSN())
+	if err != nil {
+		e.Logger.Fatal(err)
+	}
+	defer db.Close()
+
+	// migrate tables
+	if err := migration.MigrateTables(db.DB); err != nil {
+		e.Logger.Fatal(err)
+	}
+
+	// setup repository
+	repo := repository.New(db)
+
+	// setup routes
+	h := handler.New(repo)
+	v1API := e.Group("/api/v1")
+	h.SetupRoutes(v1API)
+
+	e.Logger.Fatal(e.Start(config.AppAddr()))
+}
